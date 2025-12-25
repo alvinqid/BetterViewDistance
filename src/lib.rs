65e9443fe87e3295a06555042f67fd7cf37189e8
AlvinQID <github@alvinq.my.id>
2025-12-25 09:47:05 +0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,25 @@
 mod test;
 mod preloader;
 
+pub fn setup_logging() {
+    android_logger::init_once(
+        android_logger::Config::default().with_max_level(log::LevelFilter::Trace),
+    );
+}
+
 #[ctor::ctor]
 fn safe_setup() {
-    std::panic::set_hook(Box::new(move |_panic_info| {}));
+    setup_logging();
+    std::panic::set_hook(Box::new(move |panic_info| {
+        log::error!("Thread crashed: {}", panic_info);
+    }));
     main();
 }
 
 fn main() {
-    let _ = test::write_test_to_external_dir();
+    log::info!("the main function of the BetterViewDistance mod is called!");
+    let dir_ptr = crate::preloader::pl_get_externalFiles_dir();
+    log::info!("ExternalDir: {}", dir_ptr);
 }
+
 
